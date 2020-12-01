f9b99e20fc70b31c15ddcfac43adcd04089251ce
Bharat Bhatia <b.bhatia083@gmail.com>
2020-12-01 16:27:12 +0530

    imported form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Todolist from './Components/TodoList'
+import Todolist from './Components/TodoList';
+import Todoform from './Components/Todoform'
 
 function App() {
   return (
     <div className="App">
       <Todolist></Todolist>
+      <Todoform></Todoform>
     </div>
   );
 }
